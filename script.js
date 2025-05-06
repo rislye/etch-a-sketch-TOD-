e4ccd3a02c912f4b6b6ae41561698d8cd0f62638
Rislye <158246120+rislye@users.noreply.github.com>
2025-05-06 22:26:52 +0200

    cleaned the code added rainbow and black

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-const newDiv = document.createElement("div");
 let container = document.querySelector('.container');
 let button = document.querySelector('button');
 let input = document.querySelector('#inputRange');
-let value = document.querySelector('#value');
+let value = document.querySelector('.value');
 let resetButton = document.querySelector('#resetButton');
+let rainbow = document.querySelector('.rainbow');
+let black = document.querySelector('.black');
 
+function randomRgbaString (alpha) {
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    let a = alpha
+    return `rgba(${r},${g},${b},${a})`
+}
+
+// input range display value
+value.textContent = `GRID : ${input.value}x${input.value}`
+input.addEventListener('click', () => {
+    value.textContent = `GRID : ${input.value}x${input.value}`
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    } 
+createRow()
+})
 
 function createRow() {
     const gridSizeChoice = input.value * input.value;
     const gridHeight = 960 / input.value;
-    
-    
     for(let i = 0; i < gridSizeChoice; i++) {
-        
         const newDiv = document.createElement("div");
-        newDiv.classList.add('sketch')
-        container.appendChild(newDiv)
+        container.appendChild(newDiv);
+        newDiv.classList.add('sketch');
         newDiv.style.height = `${(parseInt(gridHeight))}px`;
         newDiv.style.width = `${(parseInt(gridHeight))}px`;
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor= "black";
-        })
+            rainbow.addEventListener('click', () => {
+            newDiv.addEventListener('mouseover', () => {
+                newDiv.style.backgroundColor= randomRgbaString(1);  
+            })
+            })
+            black.addEventListener('click', () => {
+                newDiv.addEventListener('mouseover', () => {
+                    newDiv.style.backgroundColor= 'black';   
+                })
+                })
     }
-    
 }
-
-
-
-button.addEventListener('click', () => {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-} 
-    createRow()
-})
-
-
-
-
-
-
-
-
-
-
-
-
